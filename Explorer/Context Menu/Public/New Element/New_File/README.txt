74c15f24a4dcaffe2e932eb3874953ac94aae3e1
Mistkeithy <mail@mistkeith.com>
2023-03-18 17:06:15 +0600

    Context Menu updated

---
+++ Explorer/Context Menu/Public/New Element/New_File/README.txt
@@ -0,0 +1,12 @@
+UTF-8
+[EN]
+Type of files:
+
+.TXT - Text Document
+.RTF - WordPad Text Document
+
+[RU]
+Типы файлов:
+
+.TXT - Текстовый документ
+.RTF - Документ WordPad
---
+++ Explorer/Context Menu/Public/Send To/EDIT_THIS_MENU.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem [EN] We do not use .lnk links for application run by principles and security reasons to make this code transparent.
+rem [RU] Мы не используем ярлыки формата .lnk для запуска приложений по ряду принципов и соображений безопасности, чтобы сделать этот код прозрачным.
+explorer shell:sendto
